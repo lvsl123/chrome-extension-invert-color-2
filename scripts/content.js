a12964d881b01611754d157bf5910836a0034caa
lvsl123 <72800908+lvsl123@users.noreply.github.com>
2024-01-29 02:28:50 +0800

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -2,11 +2,3 @@
 
 document.body.style.backgroundColor = "black";
 document.body.style.filter = "invert(1)";
-
-
-for (let img of document.querySelectorAll("img")) {
-  img.style.filter = "invert(1)"
-}
-for (let video of document.querySelectorAll("video")) {
-  video.style.filter = "invert(1)"
-}
